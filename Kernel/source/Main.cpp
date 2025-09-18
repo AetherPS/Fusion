cc7be84ca33e206aca5e8942572f16d3ed931d73
OSM-Made <oldskoolmodz@gmail.com>
2025-09-18 16:29:32 -0600

    Remove tests

--- Kernel/source/Main.cpp
+++ Kernel/source/Main.cpp
@@ -9,105 +9,6 @@
 
 uint8_t* KernelBase;
 
-bool ShouldPrint(int entry)
-{
-	if (entry < 0x78010100)
-		return false;
-
-	switch (entry)
-	{
-	case 0x78026400:
-	case 0x7802AF00:
-	case 0x2020000:
-	case 0x5020000:
-	case 0x9400200:
-	case 0x20800000:
-	case 0x9400300:
-	case 0x20A0000:
-	case 0x9C00100:
-	case 0x2110000:
-	case 0x9C04100:
-	case 0x5010000:
-	case 0x7802B400:
-	case 0x14190600:
-		return false;
-
-	default:
-		return true;
-	}
-}
-
-int dispTitleId = 1;
-int screenshot = 0;
-int fakeVersion = 1;
-
-Detour32* sceRegMgrGetIntDetour;
-int sceRegMgrGetIntHook(int entry, int* valueOut)
-{
-	if (ShouldPrint(entry))
-		kprintf("sceRegMgrGetIntHook: %llX\n", entry);
-
-	// SCE_REGMGR_ENT_KEY_DEVENV_TOOL_SHELLUI_screenshot
-	if (entry == 0x7802C914)
-	{
-		*valueOut = screenshot;
-		return 0;
-	}
-	
-	// SCE_REGMGR_ENT_KEY_DEVENV_TOOL_SHELLUI_disp_titleid
-	if (entry == 0x7802C916)
-	{
-		*valueOut = dispTitleId;
-		return 0;
-	}
-
-	// SCE_REGMGR_ENT_KEY_NP_fake_version
-	if (entry == 0x19A50000)
-	{
-		*valueOut = fakeVersion;
-		return 0;
-	}
-
-	return sceRegMgrGetIntDetour->Invoke<int>(entry, valueOut);
-}
-
-Detour32* sceRegMgrSetIntDetour;
-int sceRegMgrSetIntHook(int entry, int value)
-{
-	if (ShouldPrint(entry))
-		kprintf("sceRegMgrSetIntHook: %llX\n", entry);
-
-	// SCE_REGMGR_ENT_KEY_DEVENV_TOOL_SHELLUI_screenshot
-	if (entry == 0x7802C914)
-	{
-		screenshot = value;
-		return 0;
-	}
-
-	// SCE_REGMGR_ENT_KEY_DEVENV_TOOL_SHELLUI_disp_titleid
-	if (entry == 0x7802C916)
-	{
-		dispTitleId = value;
-		return 0;
-	}
-
-	// SCE_REGMGR_ENT_KEY_NP_fake_version
-	if (entry == 0x19A50000)
-	{
-		fakeVersion = value;
-		return 0;
-	}
-
-	return sceRegMgrSetIntDetour->Invoke<int>(entry, value);
-}
-
-Detour32* sceRegMgrNonSysGetIntDetour;
-int sceRegMgrNonSysGetInt(uint64_t key, int a2, int* valueOut)
-{
-	kprintf("sceRegMgrNonSysGetInt: %llX\n", key);
-	return sceRegMgrNonSysGetIntDetour->Invoke<int>(key, a2, valueOut);
-}
-
 extern "C" int _main(uint64_t* p)
 {
 	KernelBase = (uint8_t*)Readmsr(0xC0000082) - (addr_Xfast_syscall + 0xFFFFFFFF82200000);
@@ -118,9 +19,6 @@ extern "C" int _main(uint64_t* p)
 
 	kprintf("Hello, World! We are now #Root.\n");
 	PrintFeatureFlags();
-
-	// sceRegMgrGetIntDetour = new Detour32(KernelBase + 0x4E9DD0, sceRegMgrGetIntHook);
-	// sceRegMgrSetIntDetour = new Detour32(KernelBase + 0x4E8B10, sceRegMgrSetIntHook);
 
 	kprintf("Initializing Watcher...");
 	Watcher::Init();
