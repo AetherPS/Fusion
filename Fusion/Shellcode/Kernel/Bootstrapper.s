aad829cea19eba643a8ccde5eef690ac7a1c0af9
OSM-Made <oldskoolmodz@gmail.com>
2026-02-06 16:48:27 -0700

    Fix a stack alignment issue.

--- Fusion/Shellcode/Kernel/Bootstrapper.s
+++ Fusion/Shellcode/Kernel/Bootstrapper.s
@@ -1,6 +1,5 @@
 BITS 64
 DEFAULT REL
-
 entry: dq shellcode
 sceKernelLoadStartModule: dq 0
 sceKernelStopUnloadModule: dq 0
@@ -9,10 +8,19 @@ ModulePath: db '/data/Fusion/Plugins/Bootstrapper.sprx', 0
 
 ; Main shellcode function
 shellcode:
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 8              ; Align stack to 16 bytes
+    
     ; Tail call to original sceSysmodulePreloadModuleForLibkernel
     mov rax, qword [sceSysmodulePreloadModuleForLibkernel]
     call rax
-
+    
     ; Load the module
     xor r9, r9
     xor r8, r8
@@ -22,7 +30,8 @@ shellcode:
     lea rdi, [ModulePath]
     mov rax, qword [sceKernelLoadStartModule]
     call rax
-
+    
+    ; Unload the module (rax contains module handle from load)
     xor r9, r9
     xor r8, r8
     xor rcx, rcx
@@ -31,5 +40,13 @@ shellcode:
     mov rdi, rax
     mov rax, qword [sceKernelStopUnloadModule]
     call rax
-
+    
+    ; Restore stack and return
+    add rsp, 8
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
     ret
